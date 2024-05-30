0c2c61aaa531e5e444f9a8f68a8eaa908e66bbb3
Smit Parmar <smit.parmar999@gmail.com>
2024-05-30 18:35:54 +0530

    changes made into forgot-password and added spinner

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -5,9 +5,8 @@ import { Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { of, throwError } from 'rxjs';
-
 
 @Component({
   selector: 'app-forgot-password',
@@ -16,11 +15,12 @@ import { of, throwError } from 'rxjs';
 })
 export class ForgotPasswordComponent {
 
-  forgotFrom !: FormGroup;
+  forgotFrom!: FormGroup;
   email: string = '';
   loading: boolean = false;
 
-  constructor(private fb: FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     private userService: UserService,
     private router: Router,
     private toaster: ToastrService
@@ -28,64 +28,56 @@ export class ForgotPasswordComponent {
 
   ngOnInit(): void {
     this.forgotFrom = this.fb.group({
-      email: ['', [Validators.required,
-      Validators.email,
-      Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]]
-    })
+      email: ['', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+      ]]
+    });
   }
 
-
-  // async onSubmit() {
-  //   if(this.forgotFrom.invalid){
-  //     return;
-  //   }
-
-  //   console.log(this.forgotFrom.value.email);
-  //   const email = this.forgotFrom.value.email;
-  //   try {
-  //     const res = await firstValueFrom(this.userService.sendOtp(email));
-  //     console.log(res);
-  //     this.router.navigate(['otp-validation'], { queryParams: { email } });
-  //     this.toaster.info("OTP has been sent to your email");
-  //   } catch (err) {
-  //     console.log("Error occurred", err);
-  //     //alert('Error occurred while sending OTP!');
-  //     this.toaster.error("Email doesn't exist","Error");
-  //   }
-
-  // }
-
   async onSubmit() {
+    
+    if (this.forgotFrom.invalid || this.loading) {
+      return;
+    }
+
+    this.loading = true;
     console.log(this.forgotFrom.value.email);
     const email = this.forgotFrom.value.email;
+
     try {
-        const res = await firstValueFrom(
-            this.userService.sendOtp(email).pipe(
-                catchError((error: any) => {
-                    if (error instanceof HttpErrorResponse) {
-                        if (error.status === 404) {
-                            this.toaster.error("Invalid email");
-                            return of(null); // Prevent further error handling
-                        } else {
-                            this.toaster.error("Error occurred while sending OTP!");
-                            return throwError(error);
-                        }
-                    } else {
-                        this.toaster.error("An unexpected error occurred!");
-                        return throwError(error);
-                    }
-                })
-            )
-        );
+      const res = await firstValueFrom(
+        this.userService.sendOtp(email).pipe(
+          catchError((error: any) => {
+            if (error instanceof HttpErrorResponse) {
+              if (error.status === 404) {
+                this.toaster.error("Invalid email");
+                this.loading = false;
+                return of(null); // Prevent further error handling
+              } else {
+                this.toaster.error("Error occurred while sending OTP!");
+                this.loading = false;
+                return throwError(error);
+              }
+            } else {
+              this.toaster.error("An unexpected error occurred!");
+              this.loading = false;
+              return throwError(error);
+            }
+          })
+        )
+      );
 
-        if (res && res.message === 'OTP has been sent to your email') {
-            console.log(res);
-            this.router.navigate(['otp-validation'], { queryParams: { email } });
-            this.toaster.info("OTP has been sent to your email");
-        }
+      if (res && res.message === 'OTP has been sent to your email') {
+        console.log(res);
+        this.router.navigate(['otp-validation'], { queryParams: { email } });
+        this.toaster.info("OTP has been sent to your email");
+      }
     } catch (err) {
-        console.log("Error occurred", err);
+      console.log("Error occurred", err);
+    } finally {
+      this.loading = false;
     }
-}
-
+  }
 }
